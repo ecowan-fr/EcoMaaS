6545ed1d2cf54964338c7f91dddd8a557a3a707c
LiamTHCH <liam@liamtc.tech>
2024-06-22 20:04:51 +0200

    chore: Comment out code for connecting to MaaS API in Clients/apps.py

--- EcOMaaS/Clients/apps.py
+++ EcOMaaS/Clients/apps.py
@@ -1,6 +1,5 @@
 from django.apps import AppConfig
 from .models import  LoginForm, api, MaaS, connect, disconnect
-import sys
 
 
 
@@ -8,11 +7,11 @@ import sys
 class ClientsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'Clients'
-    def ready(self):
-        print("ready", file=sys.stderr) # Print ready
- #       object = MaaS.objects.all() # Get all the MaaS objects
- #       for i in object: # Loop through all the MaaS objects
- #           i.connect() # Connect to the MaaS API
- #           print("connected", file=sys.stderr) # Print connected
+
+
 
 # run at startup
+#object = MaaS.objects.all() # Get all the MaaS objects
+#for i in object: # Loop through all the MaaS objects
+#    i.connect() # Connect to the MaaS API
+#    print("connected", file=sys.stderr) # Print connected
