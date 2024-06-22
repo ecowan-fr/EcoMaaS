c943cf12836c496dacda5f665e0844ace39730da
LiamTHCH <liam@liamtc.tech>
2024-06-22 16:32:52 +0200

    chore: Update import_db.py to use mysql-connector-python

---
+++ EcOMaaS/import_db.py
@@ -0,0 +1,40 @@
+import mysql.connector
+import os
+# Database connection details
+config = {
+    'user': os.environ.get("SQL_USER", "user"),
+    'password': os.environ.get("SQL_PASSWORD", "password"),
+    'host': os.environ.get("SQL_HOST", "localhost"),
+    'database': os.environ.get("SQL_DATABASE", BASE_DIR / "db.sqlite3"),
+}
+
+# Path to your schema dump file
+schema_dump_file = 'schema.sql'
+
+# Establish a connection to the database
+try:
+    connection = mysql.connector.connect(**config)
+    cursor = connection.cursor()
+
+    # Read the schema dump file
+    with open(schema_dump_file, 'r') as file:
+        schema_sql = file.read()
+
+    # Execute the schema SQL
+    for result in cursor.execute(schema_sql, multi=True):
+        if result.with_rows:
+            print("Rows produced by statement '{}':".format(result.statement))
+            print(result.fetchall())
+        else:
+            print("Number of rows affected by statement '{}': {}".format(result.statement, result.rowcount))
+
+    # Commit the transaction
+    connection.commit()
+
+except mysql.connector.Error as err:
+    print(f"Error: {err}")
+finally:
+    if connection.is_connected():
+        cursor.close()
+        connection.close()
+        print("Connection closed.")
